690b2058fa8d11245e1862279a09f6009af3b458
nurmoohamedi <nurmoohamedi@gmail.com>
2026-09-03 17:14:43 +0500

    INIT: Инициализирован проект game-api со структурой и подключением к PostgreSQL через GORM

---
+++ models/models.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Game struct {
+	gorm.Model
+	Title string `json:"title"`
+	Characters []Character `json:"characters,omitempty"`
+}
+
+type Character struct {
+	gorm.Model
+	Name string `json:"name"`
+	Class string `json:"class"`
+	GameID uint `json:"game_id"`
+}
